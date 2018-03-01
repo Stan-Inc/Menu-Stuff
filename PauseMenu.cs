c084ac6a0b62f543d642e849c9013ad928039e31
Satan-Inc <36431133+Satan-Inc@users.noreply.github.com>
2018-02-28 20:48:35 -0500

    Add files via upload

---
+++ PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = true;
+    public GameObject PauseMenuUI;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+   public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+   public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        EditorSceneManager.LoadScene("Menu1");
+        Debug.Log("Loading Menu...");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting Game...");
+    }
+}
